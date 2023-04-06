92687b266701216329e91eac255822ec8a5b6cb1
murphyd <murphyd@newmanu.edu>
2023-04-06 12:21:12 -0500

    Removed CK editor and added Tiptap editor

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -1,9 +1,18 @@
 'use strict';
 
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+/* eslint-disable no-unused-vars */
+module.exports = (config, webpack) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack config
+  // Important: return the modified config
 
-module.exports = (config) => {
-  config.plugins.push(new MonacoWebpackPlugin());
+  config.plugins.push(new webpack.NormalModuleReplacementPlugin(
+    /^tippy\.js$/,
+    'tippy.js/dist/tippy-bundle.umd.min.js'
+  ))
 
   return config;
 };
+
+
+
